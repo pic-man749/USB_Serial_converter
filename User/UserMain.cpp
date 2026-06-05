79645f28a00afae0585d95e27b775ce2f343bc58
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2026-06-05 21:04:55 +0900

    initial commit

---
+++ User/UserMain.cpp
@@ -0,0 +1,18 @@
+/*
+ * UserMain.cpp
+ *
+ *      Author: picman
+ */
+#include "UserMain.hpp"
+
+namespace {
+
+} // namespace
+
+void setup(void) {
+
+}
+
+void loop(void) {
+
+}
---
+++ User/UserMain.hpp
@@ -0,0 +1,23 @@
+/*
+ * UserMain.hpp
+ *
+ *      Author: picman
+ */
+
+#ifndef USER_INC_USERMAIN_HPP_
+#define USER_INC_USERMAIN_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* USER_INC_USERMAIN_HPP_ */
